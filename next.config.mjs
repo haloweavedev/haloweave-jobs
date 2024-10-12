05416fa8bd938611d643801b9df23ca688caad8e
haloweave <haloweavedev@gmail.com>
2024-10-12 05:47:58 +0530

    trying to edit next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@prisma/client'],
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
+      config.plugins = [...config.plugins, new PrismaPlugin()]
     }
-    return config;
-  },
-};
 
-export default nextConfig;
+    return config
+  }
+}
+
+export default nextConfig
